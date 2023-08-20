6b40ddb75a47e13feefac9018b3964bfd7507a26
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-20 14:28:04 +0200

    add fetch posts

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,7 +1,18 @@
 import React from "react"
+import { useEffect, useState } from "react"
 
 const PostList = () => {
-  return <div>PostList</div>
+  const [postList, setPostList] = useState([])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => setPostList(posts))
+  }, [])
+  return (
+    <div>
+      <pre>{JSON.stringify(postList, undefined, 2)}</pre>
+    </div>
+  )
 }
 
 export default PostList
