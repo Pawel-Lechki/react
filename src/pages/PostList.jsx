4ec2275fc3c684fa5f119ba73e7866a8bc474fd6
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-20 16:20:41 +0200

    add delete post

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -9,11 +9,16 @@ const PostList = () => {
       .then((res) => res.json())
       .then((posts) => setPostList(posts))
   }, [])
+
+  function onDelete(post) {
+    setPostList(postList.filter((p) => p.id != post.id))
+  }
+
   return (
     <div>
       {/* <pre>{JSON.stringify(postList, undefined, 2)}</pre> */}
       {postList.map((post) => (
-        <PostItem key={post.id} post={post} />
+        <PostItem key={post.id} post={post} onDelete={onDelete} />
       ))}
     </div>
   )
